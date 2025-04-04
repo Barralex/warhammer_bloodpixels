e3e6dfe5cf1e84cafec9fb2e2905ddb9ac979cc6
Luis Barral <lbarral@vairix.com>
2025-04-04 14:44:41 -0300

    Add UnitConfig class and refactor Unit initialization for better configuration management

--- lib/models/unit.dart
+++ lib/models/unit.dart
@@ -1,3 +1,23 @@
+class UnitConfig {
+  final int hp;
+  final int maxHp;
+  final int movement;
+  final int attackRange;
+  final int chargeRange;
+  final int damage;
+  final int weaponBS;
+
+  const UnitConfig({
+    required this.hp,
+    required this.maxHp,
+    required this.movement,
+    required this.attackRange,
+    required this.chargeRange,
+    required this.damage,
+    required this.weaponBS,
+  });
+}
+
 class Unit {
   final String type;
   int hp;
@@ -8,12 +28,34 @@ class Unit {
   final int damage;
   final int weaponBS;
 
+  static const Map<String, UnitConfig> unitConfigs = {
+    'space_marine': UnitConfig(
+      hp: 10,
+      maxHp: 10,
+      movement: 6,
+      attackRange: 3,
+      chargeRange: 12,
+      damage: 3,
+      weaponBS: 3,
+    ),
+    'tyranid': UnitConfig(
+      hp: 5,
+      maxHp: 5,
+      movement: 6,
+      attackRange: 6,
+      chargeRange: 12,
+      damage: 2,
+      weaponBS: 4,
+    ),
+  };
+
   Unit(this.type)
-    : hp = type == 'space_marine' ? 10 : 5,
-      maxHp = type == 'space_marine' ? 10 : 5,
-      movement = type == 'space_marine' ? 6 : 6,
-      attackRange = type == 'space_marine' ? 3 : 6,
-      chargeRange = 12,
-      damage = type == 'space_marine' ? 3 : 2,
-      weaponBS = type == 'space_marine' ? 3 : 4;
+    : assert(unitConfigs.containsKey(type), 'Unknown unit type: $type'),
+      hp = unitConfigs[type]!.hp,
+      maxHp = unitConfigs[type]!.maxHp,
+      movement = unitConfigs[type]!.movement,
+      attackRange = unitConfigs[type]!.attackRange,
+      chargeRange = unitConfigs[type]!.chargeRange,
+      damage = unitConfigs[type]!.damage,
+      weaponBS = unitConfigs[type]!.weaponBS;
 }
